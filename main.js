b8ef1694e5235d8e4b1722a2ee0f4219bc497c1c
ammo20rana <167557167+ammo20rana@users.noreply.github.com>
2024-04-24 19:54:41 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+import inquirer from "inquirer";
+let mybalance = 10000; //dollar
+let mypin = 1234;
+let pinanswer = await inquirer.prompt({
+    name: "pin",
+    message: "enter your pin",
+    type: "number",
+});
+if (pinanswer.pin === mypin) {
+    console.log("correct pin code!!!");
+    let operationans = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "please select option",
+            type: "list",
+            choices: ["withdraw", "check balance"],
+        }
+    ]);
+    console.log(operationans);
+    if (operationans.operation === "withdraw") {
+        let amountans = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number",
+            }
+        ]);
+        mybalance -= amountans.amount;
+        console.log("your remainning balance is:" + mybalance);
+    }
+    else if (operationans.operation === "check balance") {
+        console.log("your balance is:" + mybalance);
+    }
+}
+else {
+    console.log("incorrect pin code!!!");
+}
